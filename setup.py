9e7a26b11eca490ce86b26d99e71fd48ef2c3e7b
Anton Chernyatevich <profa142@gmail.com>
2023-06-13 20:32:36 +0300

    Add vanilla CI and change setup

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
 from setuptools import setup
 
-setup()
+VERSION: dict[str, str] = {}
+
+with open("arrafire/version.py", "r") as version_file:
+    exec(version_file.read(), VERSION)
+
+setup(version=VERSION["VERSION"])
