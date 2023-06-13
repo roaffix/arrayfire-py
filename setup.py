7017ac5f2cec21f27c793cf678831a1ea4da476c
Anton Chernyatevich <profa142@gmail.com>
2023-06-13 21:02:34 +0300

    Change setup

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
+from pathlib import Path
+
 from setuptools import setup
 
 VERSION: dict[str, str] = {}
 
-with open("arrafire/version.py", "r") as version_file:
+
+with (Path().absolute() / "arrayfire" / "version.py").open("r") as version_file:
     exec(version_file.read(), VERSION)
 
 setup(version=VERSION["VERSION"])
