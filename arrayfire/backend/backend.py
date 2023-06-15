000d168bbe9ce3d4f5b7b9ed63b136d1b3ad0e11
Anton <profa142@gmail.com>
2023-06-15 20:05:15 +0300

    Feature/array object refactoring (#2)
    * Array object refactoring
    
    * Structure refactoring
    
    * Index.py and broadcast.py refactoring
    
    * Fix imports and typings
    
    * Add comments for placed hacks

---
+++ arrayfire/backend/backend.py
@@ -0,0 +1,29 @@
+import ctypes
+import enum
+from dataclasses import dataclass
+
+from ..dtypes.helpers import c_dim_t, to_str
+
+# HACK for osx
+# backend_api = ctypes.CDLL("/opt/arrayfire//lib/libafcpu.3.dylib")
+# HACK for windows
+backend_api = ctypes.CDLL("C:/Program Files/ArrayFire/v3/lib/afcpu.dll")
+
+
+def safe_call(c_err: int) -> None:
+    if c_err == _ErrorCodes.none.value:
+        return
+
+    err_str = ctypes.c_char_p(0)
+    backend_api.af_get_last_error(ctypes.pointer(err_str), ctypes.pointer(c_dim_t(0)))
+    raise RuntimeError(to_str(err_str))
+
+
+class _ErrorCodes(enum.Enum):
+    none = 0
+
+
+@dataclass
+class ArrayBuffer:
+    address: int
+    length: int = 0
