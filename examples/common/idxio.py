b91de588e1ce01de91cf05360eef7b0c9ec85131
Chaluvadi <saketh.chaluvadi@intel.com>
2024-03-25 23:22:02 -0400

    added common example

--- examples/common/idxio.py
+++ examples/common/idxio.py
@@ -9,6 +9,7 @@
 # http://arrayfire.com/licenses/BSD-3-Clause
 ########################################################
 
+
 def reverse_char(b):
     b = (b & 0xF0) >> 4 | (b & 0x0F) << 4
     b = (b & 0xCC) >> 2 | (b & 0x33) << 2
@@ -18,23 +19,23 @@ def reverse_char(b):
 
 # http://stackoverflow.com/a/9144870/2192361
 def reverse(x):
-    x = ((x >> 1)  & 0x55555555) | ((x & 0x55555555) << 1)
-    x = ((x >> 2)  & 0x33333333) | ((x & 0x33333333) << 2)
-    x = ((x >> 4)  & 0x0f0f0f0f) | ((x & 0x0f0f0f0f) << 4)
-    x = ((x >> 8)  & 0x00ff00ff) | ((x & 0x00ff00ff) << 8)
-    x = ((x >> 16) & 0xffff)     | ((x & 0xffff)     << 16);
+    x = ((x >> 1) & 0x55555555) | ((x & 0x55555555) << 1)
+    x = ((x >> 2) & 0x33333333) | ((x & 0x33333333) << 2)
+    x = ((x >> 4) & 0x0F0F0F0F) | ((x & 0x0F0F0F0F) << 4)
+    x = ((x >> 8) & 0x00FF00FF) | ((x & 0x00FF00FF) << 8)
+    x = ((x >> 16) & 0xFFFF) | ((x & 0xFFFF) << 16)
     return x
 
 
 def read_idx(name):
-    with open(name, 'rb') as f:
+    with open(name, "rb") as f:
         # In the C++ version, bytes the size of 4 chars are being read
         # May not work properly in machines where a char is not 1 byte
         bytes_read = f.read(4)
         bytes_read = bytearray(bytes_read)
 
         if bytes_read[2] != 8:
-            raise RuntimeError('Unsupported data type')
+            raise RuntimeError("Unsupported data type")
 
         numdims = bytes_read[3]
         elemsize = 1
@@ -48,11 +49,11 @@ def read_idx(name):
             # Big endian to little endian
             for j in range(4):
                 bytes_read[j] = reverse_char(bytes_read[j])
-            bytes_read_int = int.from_bytes(bytes_read, 'little')
+            bytes_read_int = int.from_bytes(bytes_read, "little")
             dim = reverse(bytes_read_int)
 
-            elem = elem * dim;
-            dims[i] = dim;
+            elem = elem * dim
+            dims[i] = dim
 
         # Read the data
         cdata = f.read(elem * elemsize)
@@ -61,13 +62,14 @@ def read_idx(name):
 
         return (dims, data)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # Example usage of reverse_char
-    byte_value = 0b10101010  
+    byte_value = 0b10101010
     reversed_byte = reverse_char(byte_value)
     print(f"Original byte: {byte_value:08b}, Reversed byte: {reversed_byte:08b}")
 
     # Example usage of reverse
-    int_value = 0x12345678  
+    int_value = 0x12345678
     reversed_int = reverse(int_value)
     print(f"Original int: {int_value:032b}, Reversed int: {reversed_int:032b}")
