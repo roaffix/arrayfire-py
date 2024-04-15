2967856f245ef2d8df0dd85dbe0b6f9dddb612df
sakchal <chaluvadi.saketh@gmail.com>
2024-04-15 17:41:48 -0400

    benchmark fixes

--- examples/benchmarks/bench_fft.py
+++ examples/benchmarks/bench_fft.py
@@ -1,3 +1,11 @@
+#!/usr/bin/python
+
+#######################################################
+# Copyright (c) 2024, ArrayFire
+# All rights reserved.
+#
+# This file is distributed under 3-clause BSD license.
+# The complete license agreement can be obtained at:
 # http://arrayfire.com/licenses/BSD-3-Clause
 ########################################################
 
