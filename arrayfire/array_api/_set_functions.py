20b46eb4188baa9c9b82fc0f99ab0d5f9aa057b8
Anton <profa142@gmail.com>
2024-04-16 18:53:31 +0300

    Remove bugged code from set functions

--- arrayfire/array_api/_set_functions.py
+++ arrayfire/array_api/_set_functions.py
@@ -23,25 +23,6 @@ class UniqueInverseResult(NamedTuple):
 
 
 def unique_all(x: Array, /) -> UniqueAllResult:
-    # BUG, FIXME
-    # array_ = af.flat(x._array) if len(x.shape) != 1 else x._array
-
-    # sorted_array, original_indices = af.sort(array, is_ascending=True)
-    # unique_elements, unique_counts = af.set_unique(sorted_array, is_sorted=True)
-
-    # # Find indices of unique elements in the sorted array
-    # _, unique_indices_sorted = af.setunique(sorted_array, is_sorted=True, is_index=True)
-
-    # # Map indices in the sorted array back to original indices
-    # indices = af.gather(original_indices, unique_indices_sorted, 0)
-
-    # # Generate inverse indices
-    # _, inverse_indices = af.setunique(original_indices, is_sorted=False, is_index=True)
-
-    # # Counts of each unique element are directly obtained from setunique
-    # counts = unique_counts
-
-    # return UniqueAllResult(unique_elements, indices, inverse_indices, counts)
     return NotImplemented
 
 
